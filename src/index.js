c88346c5e9dc1ff4f01c849481f45f8f1578b2a8
takeuchi <48384909+d-takeuchi@users.noreply.github.com>
2021-07-12 21:08:53 +0900

    バニラjsでtodoアプリの作成

--- src/index.js
+++ src/index.js
@@ -1,43 +1,77 @@
-// /**
-//  * Mapやfilterを使った配列の処理
-//  */
-
-// const nameArr = ["田中", "山田", "じゃけぇ"];
-
-// //従来の場合
-// // for (let i = 0; i < nameArr.length; i++) {
-// //   console.log(nameArr[i]);
-// // }
-
-// // const nameArr2 = nameArr.map((name) => console.log(name));
-
-// // const numArr = [1, 2, 3, 4, 5];
-
-// // const newNumArr = numArr.filter((num) => num % 2 === 1);
-
-// // console.log(newNumArr);
-
-// const newNameArr = nameArr.map((name) => {
-//   if (name === "じゃけぇ") {
-//     return name;
-//   } else {
-//     return `${name}さん`;
-//   }
-// });
-
-// console.log(newNameArr)
-
-// const val1 = 1 > 0 ? true : false;
-// console.log(val1);
-
-// const num = 1300;
-
-// const formattedNum =
-//   typeof num === "number" ? num.toLocaleString() : "数値で入力してください";
-// console.log(formattedNum);
-
-const checkSum = (num1, num2) => {
-  return num1 + num2 > 100 ? "100を超えています。" : "許容範囲内です。";
+//未完了リストから指定の要素を削除
+const deleteFromInCompleteList = (target) => {
+  document.getElementById("incomplete-list").removeChild(target);
 };
 
-console.log(checkSum(98, 2));
+// 未完了リストに追加する関数
+const createIncompleteList = (text) => {
+  // divタグの生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  // liタグの生成
+  const li = document.createElement("li");
+  li.innerText = text;
+  div.appendChild(li);
+
+  // button（完了）タグ生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    deleteFromInCompleteList(completeButton.parentNode);
+    //完了リストに要素を追加
+    const addTarget = completeButton.parentNode;
+    // TODO内容テキストを取得
+    const text = addTarget.firstElementChild.innerText;
+
+    // div以下を初期化
+    addTarget.textContent = null;
+
+    // liタグ生成
+    const li = document.createElement("li");
+    li.innerText = text;
+
+    // buttonタグ生成
+    const backButton = document.createElement("button");
+    backButton.innerText = "戻す";
+
+    backButton.addEventListener("click", () => {
+      const deleteTarget = backButton.parentNode;
+      document.getElementById("complete-list").removeChild(deleteTarget);
+
+      const text = deleteTarget.firstChild.innerText;
+      createIncompleteList(text);
+    });
+    addTarget.appendChild(li);
+    addTarget.appendChild(backButton);
+
+    document.getElementById("complete-list").appendChild(addTarget);
+  });
+
+  //button（削除）タグの生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    deleteFromInCompleteList(deleteButton.parentNode);
+  });
+
+  //divタグの子要素に各要素を設定
+  div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+
+  // 未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(div);
+};
+
+const onClickAdd = () => {
+  // 入力されたテキストを未完了のTODOリストに追加
+  const inputElement = document.getElementById("add-text");
+  const inputText = inputElement.value;
+  inputElement.value = "";
+  createIncompleteList(inputText);
+};
+
+document.getElementById("add-button").addEventListener("click", () => {
+  onClickAdd();
+});
